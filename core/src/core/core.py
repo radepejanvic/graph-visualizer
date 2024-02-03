1e6d4bd8de7c0ddf9539c81aee6374cb1e3344cf
Rade Pejanović <radepejanovic@gmail.com>
2024-02-03 16:06:52 +0100

    fix: Refactored typings and syntax errors

--- core/src/core/core.py
+++ core/src/core/core.py
@@ -1,14 +1,15 @@
 from abc import ABC
-from api.src.api.models import Graph
-from api.src.api.services import DataSourceBase, VisualizerBase
 from typing import List
 import pkg_resources
 
+from api.src.api.models.model import Graph
+from api.src.api.services.base import DataSourceBase, VisualizerBase
 
 DATA_SOURCES = "data_sources"
-VISUALIZERS = "data_sources"
+VISUALIZERS = "visualizers"
 
-class Core(ABC): 
+
+class Core(ABC):
 
     def __init__(self):
         self.g: Graph = Graph()
@@ -16,22 +17,22 @@ class Core(ABC):
         self.vs: List[VisualizerBase] = self._load_plugins(VISUALIZERS)
 
     @property
-    def graph(self) -> Graph: 
+    def graph(self) -> Graph:
         return self.g
 
-    @property.setter
-    def graph(self, new_graph): 
-        self.g = new_graph;  
+    @graph.setter
+    def graph(self, new_graph):
+        self.g = new_graph
 
     @property
-    def data_sources(self) -> List[DataSourceBase]: 
-        return self.ds()
+    def data_sources(self) -> List[DataSourceBase]:
+        return self.ds
 
     @property
-    def visualizers(self) -> List[VisualizerBase]: 
-        return self.vs()
-    
-    def _load_plugins(self, group: str):
+    def visualizers(self) -> List[VisualizerBase]:
+        return self.vs
+
+    def _load_plugins(self, group: str) -> list:
         plugins = []
 
         for ep in pkg_resources.iter_entry_points(group=group):
@@ -41,11 +42,9 @@ class Core(ABC):
             plugins.append(plugin)
 
         return plugins
-    
+
     def load_graph(self, data_source: int):
         self.g = self.ds[data_source].load()
 
-    def display_graph(self, visualizer: int) -> str: 
+    def display_graph(self, visualizer: int) -> str:
         return self.vs[visualizer].display(self.g)
-        
-        
