b181aa89bd03b3b7ac81f6c5ae76cfb877c09ede
Rade Pejanović <radepejanovic@gmail.com>
2024-02-04 00:40:50 +0100

    fix: Fixed graph loading bug #9

--- graph_explorer/app/urls.py
+++ graph_explorer/app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index,name="index"),
-    path('nba',views.nba,name="nba"),
+    path('', views.index, name="index"),
+    path('nba', views.nba, name="nba"),
 ]
--- graph_explorer/app/views.py
+++ graph_explorer/app/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.apps.registry import apps
 
+
 def index(request):
     core = apps.get_app_config("app").core
-    return render(request,"index.html", {"title": "Index", "plugini_ucitavanje": core.data_sources})
+    return render(request,
+                  "index.html",
+                  {"title": "Index",
+                   "data_sources": core.data_sources,
+                   "visualizers": core.visualizers})
 
 
 def nba(request):
     core = apps.get_app_config("app").core
     core.load_graph(0)
-    return render(request,"index.html", {"title": "Index", "plugini_ucitavanje": core.data_sources, "main_view": core.display_graph(0)})
+    return render(request, "index.html",
+                  {"title": "Index", "data_sources": core.data_sources, "main_view": core.display_graph(0)})
