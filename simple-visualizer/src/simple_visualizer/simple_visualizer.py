652787d89678001f4c1dbff9a4b99e9e7101fe66
Rade Pejanović <radepejanovic@gmail.com>
2024-02-04 23:18:53 +0100

    feat: Created SimpleVisualizer #4

---
+++ simple-visualizer/src/simple_visualizer/simple_visualizer.py
@@ -0,0 +1,22 @@
+from jinja2 import Environment, FileSystemLoader
+from api.src.api.services.base import VisualizerBase
+from api.src.api.models.model import *
+
+
+class SimpleVisualizer(VisualizerBase):
+
+    def identifier(self) -> str:
+        return "SimpleVisualizer"
+
+    def name(self) -> str:
+        return "Simple Visualizer"
+
+    def display(self, graph: Graph):
+        template_dir = "/simple-visualizer/src/simple_visualizer"
+        env = Environment(loader=FileSystemLoader(template_dir))
+        template = env.get_template("simple.html")
+        data = {
+            "graph": graph
+        }
+        return template.render(data)
+
