8d91f90c6c80958d0fc47094adddc193e2eb262b
Rade Pejanović <radepejanovic@gmail.com>
2024-02-04 13:03:29 +0100

    fix: Partially fixed redirect bug #9

--- graph_explorer/app/static/script.js
+++ graph_explorer/app/static/script.js
@@ -1,8 +1,9 @@
-function redirect() {
-        let visualizer = document.getElementById("visualizer_select").value;
-        let data_source = document.getElementById("data_source_select").value;
+function redirect(baseUrl) {
+    console.log(baseUrl)
+    let visualizer = document.getElementById("visualizer_select").value;
+    let data_source = document.getElementById("data_source_select").value;
 
-        if (visualizer > 0 && data_source > 0) {
-             window.location.href += `/main/${visualizer-1}/${data_source-1}`;
-        }
+    if (visualizer > 0 && data_source > 0) {
+         window.location.href = `/main/${visualizer-1}/${data_source-1}`;
     }
+}
